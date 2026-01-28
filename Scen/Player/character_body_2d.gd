2ef0451a54e0a750c303f21dbb627afd6c44638e
Krytovskiq123 <krytovskiq1@gmail.com>
2026-01-28 23:36:35 +0500

    Game666

--- Scen/Player/character_body_2d.gd
+++ Scen/Player/character_body_2d.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @onready var _animated_sprite = $AnimatedSprite2D
 @export var speed = 70
 
+var bullet = preload("res://Scen/World/bullet.gd")
+
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_direction * speed
@@ -10,8 +12,7 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
-	
-func _process(_delta):
+
 	if Input.is_action_pressed("ui_left"):
 		_animated_sprite.play("idle_right")
 	elif Input.is_action_pressed("ui_right"):
---
+++ Scen/World/bullet.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+var speed = 750
+
+func _physics_process(delta):
+	position += transform.x * speed * delta
+	
+func _on_Buller_body_entered(body):
+	if body.is_in_group("mob"):
+		body.queue_free()
+	queue_free()
